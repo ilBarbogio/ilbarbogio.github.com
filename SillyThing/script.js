dcd1dac88bfe57ec930036b5519a40882d544047
Carminati Alberto <barbogioster@gmail.com>
2023-12-20 07:43:30 +0100

    Update script.js

--- SillyThing/script.js
+++ SillyThing/script.js
@@ -12,11 +12,15 @@ let hypeDelta=10
 let hypeGoal=200
 let egg,eggInterval
 
+let ball 
 let lastVector
-let motionSequence
-let motionInterval
+let vectorHype=0
+let vectorHypeDelta=10
+let vectorHypeGoal=200
 
 function setup(){
+  let side=Math.floor(Math.random()*6+2)+"em"
+  ball=createBall(side,"red")
 	//if(ScreenOrientation.lock){
 		//ScreenOrientation.lock()
 		//.then((v)=>{
@@ -24,56 +28,68 @@ function setup(){
 		//})
 	//}else setupUnmobile()
 }
-let debug=0
+
+//devicemotion available
 function setupMobile(){
 	// egg=document.createElement("div")
 	// egg.id="tap-egg"
 	// document.body.append(egg)
 
-	text=document.createElement("p")
-	text.classList.add("fire")
-	document.body.append(text)
-  text.innerHTML="ciao"
 
-	window.addEventListener("devicemotion",ev=>{
-    let intensity=Math.hypot(ev.acceleration.x,ev.acceleration.y,ev.acceleration.z,debug)
-    debug=Math.max(intensity,debug)
-	  text.innerHTML=`${debug} <br> ${ev.acceleration.x},  ${ev.acceleration.y},  ${ev.acceleration.z}`
-  })
+	window.addEventListener("devicemotion",calibrateMotion)
 }
 
 function calibrateMotion(ev){
-	window.removeListener("devicemotion",calibrateMotion)
+	window.removeEventListener("devicemotion",calibrateMotion)
+  vectorHype=0
 	lastVector=undefined
-	motionSequence=[]
-	motionInterval=setInterval(registerMotion,ev.interval)
+  window.addEventListener("devicemotion",registerMotion)
+	eggInterval=setInterval(subtractHype,ev.interval*5)
 }
 function registerMotion(ev){
 	if(!lastVector) lastVector=ev.acceleration
 	else{
-		let distance=Math.hypot(ev.acceleration.x-lastVector.x, ev.acceleration.y-lastVector.y, ev.acceleration.z-lastVector.z)
-		text.innerHTML=distance
-		lastVector=ev.acceleration
+		let intensity=Math.hypot(ev.acceleration.x-lastVector.x,ev.acceleration.y-lastVector.y,ev.acceleration.z-lastVector.z)
+    if(intensity>vectorHypeDelta) addHype()
+    lastVector=ev.acceleration
 	}
 }
 
 
-
+//no devicemotion
 function setupUnmobile(){
 	egg=document.createElement("div")
 	egg.id="tap-egg"
 	document.body.append(egg)
 
+  hype=0
+
 	egg.addEventListener("click",addHype)
 	eggInterval=setInterval(subtractHype,350)
 }
+
+
+
 function addHype(){
 	hype+=hypeDelta
-	console.log(hype)
-	if(hype>=hypeGoal){
-		egg.removeEventListener("click",addHype)
-		egg.removeEventListener("click",subtractHype)
-		egg.remove()
+  if(hype<hypeGoal*.2){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+  }else if(hype<hypeGoal*.4){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-small")
+  }else if(hype<hypeGoal*.6){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-med")
+  }else if(hype<hypeGoal*.8){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-big")
+  }else if(hype>=hypeGoal){
+		if(egg){
+      egg.removeEventListener("click",addHype)
+      egg.removeEventListener("click",subtractHype)
+      egg.remove()
+    }
+		window.removeEventListener("devicemotion",registerMotion)
 		clearInterval(eggInterval)
 
 		appearText()
@@ -82,10 +98,53 @@ function addHype(){
 function subtractHype(){
 	hype-=hypeDelta
 	if(hype<0) hype=0
-	console.log(hype)
+  if(hype<hypeGoal*.2){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+  }else if(hype<hypeGoal*.4){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-small")
+  }else if(hype<hypeGoal*.6){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-med")
+  }else if(hype<hypeGoal*.8){
+    ball.classList.remove("shake-small", "shake-med", "shake-big")
+    ball.classList.add("shake-big")
+  }
 }
 
 
+//graphics
+function createBall(dimensions,color){
+  let ball=document.createElement("div")
+  ball.classList.add("ball")
+  ball.style.setProperty("--ball-size",dimensions)
+  ball.style.setProperty("width",dimensions)
+  ball.style.setProperty("height",dimensions)
+  ball.style.setProperty("background-color",color)
+
+  let shine=document.createElement("div")
+  shine.classList.add("shine")
+  ball.append(shine)
+
+  let smallShine=document.createElement("div")
+  smallShine.classList.add("small-shine")
+  ball.append(smallShine)
+
+  let cap=document.createElement("div")
+  cap.classList.add("cap")
+  ball.append(cap)
+
+  let ring=document.createElement("div")
+  ring.classList.add("ring")
+  ball.append(ring)
+
+  document.body.append(ball)
+
+  return ball
+}
+
+
+//ending
 function appearText(){
 	let index=Math.floor(Math.random()*sillyPhrases.length)
 	text=document.createElement("p")
